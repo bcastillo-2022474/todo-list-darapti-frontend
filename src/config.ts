5543643dc85e351fd23838596fff738bfdaccd99
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2024-11-06 00:07:18 -0600

    fix: setting API URL

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,2 @@
 // export const API_URL = process.env['API_URL'] || 'http://localhost:3000/api/v1';
-export const API_URL =
-  'https://todo-list-backend-98844aefd33a.herokuapp.com/api/v1';
+export const API_URL = 'http://todo-list-backend-i8ufij-4309ac-116-202-105-230.traefik.me/api/v1';
